122b60004019d18c79281ea39c69c3736526a463
vinandra <vinandra207@gmail.com>
2024-03-06 23:56:13 +0700

    add: markdown-to-jsx

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,20 @@
+import fs from "fs";
+import Markdown from "markdown-to-jsx";
+
+const getPostConstent = (slug: string) => {
+  const folder = "posts/";
+  const file = `${folder}${slug}.md`;
+  const content = fs.readFileSync(file, "utf8");
+  return content;
+};
+
 export default function PostPage(props: any) {
   const slug = props.params.slug;
-  return <p>ini adalah pose dari : {slug}</p>;
+  const content = getPostConstent(slug);
+  return (
+    <div>
+      <h1>ini adalah pose dari : {slug}</h1>
+      <Markdown>{content}</Markdown>
+    </div>
+  );
 }
